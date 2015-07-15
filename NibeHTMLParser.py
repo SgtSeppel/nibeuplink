b53cd7f5d405fb13ffd0f6edd3935b04d3f84c4b
Sebastian Kuhn <SgtSeppel@users.noreply.github.com>
2015-07-15 13:28:04 +0200

    initial commit

---
+++ NibeHTMLParser.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+#########################################################################
+#  Copyright 2015 Sebastian Kuhn                  sebastian@derseppel.net
+#########################################################################
+#  This file is part of SmartHome.py.    http://mknx.github.io/smarthome/
+#
+#  SmartHome.py is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  SmartHome.py is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with SmartHome.py. If not, see <http://www.gnu.org/licenses/>.
+#########################################################################
+
+from html.parser import HTMLParser
+import re
+
+try:
+  from http.cookiejar import CookieJar
+except ImportError:
+  from cookielib import CookieJar
+
+
+try:
+  from urllib.parse import urlencode
+  from urllib.request import build_opener
+  from urllib.request import HTTPRedirectHandler
+  from urllib.request import HTTPHandler
+  from urllib.request import HTTPSHandler
+  from urllib.request import HTTPCookieProcessor
+except ImportError:
+  import urllib2
+
+
+class NibeHTMLParser(HTMLParser):
+  def __init__(self):
+    HTMLParser.__init__(self)
+    self.inSpan = False
+    self.lasttag = None
+    self.lastname = None
+    self.lastvalue = None
+    self.lastclass = None
+    self.data = dict()
+    self.regex_sz = re.compile(r'\\{1}[a-zA-Z\S]+')
+
+  def handle_starttag(self, tag, attrs):
+    self.inSpan = False
+    self.lastclass = None
+    if tag == 'span':
+      for name, value in attrs:
+        if name == 'class' and 'AutoUpdateValue' in value:
+          self.inSpan = True
+          self.lastclass = re.findall(r'ID\d{5}', value)[0]
+          self.lasttag = tag
+
+  def handle_endtag(self, tag):
+    if tag == 'span':
+      self.inSpan = False
+
+  def handle_data(self, data):
+    if self.lasttag == 'span' and self.inSpan and data.strip():
+      strippedValue = re.sub(self.regex_sz, '', data)
+      self.data[self.lastclass] =  strippedValue
+
+  def getDataArray(self):
+    return self.data
---
+++ __init__.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+#########################################################################
+#  Copyright 2015 Sebastian Kuhn                  sebastian@derseppel.net
+#########################################################################
+#  This file is part of SmartHome.py.    http://mknx.github.io/smarthome/
+#
+#  SmartHome.py is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  SmartHome.py is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with SmartHome.py. If not, see <http://www.gnu.org/licenses/>.
+#########################################################################
+
+import logging
+from datetime import datetime, timedelta
+
+try:
+  from http.cookiejar import CookieJar
+except ImportError:
+  from cookielib import CookieJar
+
+
+try:
+  from urllib.parse import urlencode
+  from urllib.request import build_opener
+  from urllib.request import HTTPRedirectHandler
+  from urllib.request import HTTPHandler
+  from urllib.request import HTTPSHandler
+  from urllib.request import HTTPCookieProcessor
+  from urllib.request import HTTPError
+except ImportError:
+  import urllib2
+
+
+from .NibeHTMLParser import NibeHTMLParser
+
+logger = logging.getLogger('')
+
+
+class NibeUplink():
+  def __init__(self, smarthome, nibe_email, nibe_password, nibe_system, nibe_update_cyle=10):
+    logger.warn('Init NibeUplink')
+    self._sh = smarthome
+    self._items = dict()
+    self.nibe_email = nibe_email
+    self.nibe_password = nibe_password
+    self.nibe_system = nibe_system
+    self.nibe_update_cyle = nibe_update_cyle
+    self.login_url = "https://www.nibeuplink.com/Login"
+    self.service_url = "https://www.nibeuplink.com/System/"+self.nibe_system+"/Status/ServiceInfo"
+    self.cookies = CookieJar()
+    self.opener = None
+    self.parser = None
+    self.response = None
+    self._items = dict()
+      
+  def run(self):
+    self.alive = True
+    # Log into NibeUplink
+    values = {'Email' : self.nibe_email,
+          'Password' : self.nibe_password,
+          'returnUrl' : ''
+        }
+
+    data = urlencode(values)
+    binary_data = data.encode('ascii')
+    
+    self.opener = build_opener(
+         HTTPRedirectHandler(),
+         HTTPHandler(debuglevel=0),
+         HTTPSHandler(debuglevel=0),
+         HTTPCookieProcessor(self.cookies))
+    
+    self.parser = NibeHTMLParser()
+
+    try:
+      self.response = self.opener.open(self.login_url, binary_data)
+    except HTTPError as e:
+      if e.code == 401:
+      else:
+    else:
+    
+    self._sh.scheduler.add('NibeUplink', self._update_values, prio=5, cycle=self.nibe_update_cyle)
+
+  def stop(self):
+    self.alive = False
+
+  def parse_item(self, item):
+    if 'nibe_reg' in item.conf:
+      self._items[item.conf['nibe_reg']] = item
+    else:
+      return None
+
+  def update_item(self, item, caller=None, source=None, dest=None):
+    logger.warn('NibeUplink: update_item():'+item)
+    return None
+
+  def _update_values(self):
+    logger.warn('NibeUplink: I\'ve been scheduled!')
+    self.response = self.opener.open(self.service_url)
+    page = self.response.read()
+    self.parser.feed(str(page))
+    logger.warn('NibeUplink: passed the page to parser')
+    tags = self.parser.getDataArray()
+    for k,v in self._items.items():
+      item = self._items[k]
+      value = tags[k]
+      item(float(value), 'NibeUplink')
